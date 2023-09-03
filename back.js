31a539b568ca44590c5ffd9caed651e59a68b516
saicharan9502 <113277753+saicharan9502@users.noreply.github.com>
2023-09-03 12:08:10 +0530

    Add files via upload

--- back.js
+++ back.js
@@ -6,21 +6,55 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Function to check if a number is prime
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  let i = 5;
+  while (i * i <= num) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+    i += 6;
+  }
+  return true;
+}
+
 app.get('/numbers', async (req, res) => {
-  const primeUrl = 'http://20.244.56.144/numbers/primes'; // URL that generates prime numbers
+  const urls = req.query.url;
+
+  if (!urls || !Array.isArray(urls)) {
+    return res.status(400).json({ error: 'Invalid URL parameter' });
+  }
+
+  const uniquePrimes = new Set();
 
   try {
-    const response = await axios.get(primeUrl);
-    const { numbers } = response.data;
+    // Use Promise.all to concurrently fetch data from multiple URLs
+    const promises = urls.map(async (url) => {
+      try {
+        const response = await axios.get(url);
+        const { numbers } = response.data;
 
-    if (Array.isArray(numbers)) {
-      const sortedNumbers = numbers.sort((a, b) => a - b);
-      res.json({ numbers: sortedNumbers });
-    } else {
-      res.status(500).json({ error: 'Invalid response from prime numbers API' });
-    }
+        if (Array.isArray(numbers)) {
+          numbers.forEach((number) => {
+            if (isPrime(number)) {
+              uniquePrimes.add(number);
+            }
+          });
+        }
+      } catch (error) {
+        console.error(`Error fetching data from ${url}: ${error.message}`);
+      }
+    });
+
+    await Promise.all(promises);
+
+    // Convert the set of unique prime numbers to an array and sort it in ascending order
+    const sortedPrimes = [...uniquePrimes].sort((a, b) => a - b);
+
+    res.json({ numbers: sortedPrimes });
   } catch (error) {
-    console.error(`Error fetching prime numbers: ${error.message}`);
+    console.error(`Error processing numbers: ${error.message}`);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
